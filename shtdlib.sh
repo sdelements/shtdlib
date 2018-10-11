fdc69a898a10291c6d1760f1105429ce2905c71c
Hrdayesh Patel <hpatel@securitycompass.com>
2018-10-11 14:43:02 -0400

    - Add an `os_code` variable - Replace tabs with spaces

--- shtdlib.sh
+++ shtdlib.sh
@@ -51,6 +51,7 @@ init_tty="$(tty || true)"
 OS="${OS:-}"
 os_family='Unknown'
 os_name='Unknown'
+os_code='Unknown'
 apt-get help > /dev/null 2>&1 && os_family='Debian'
 yum help help > /dev/null 2>&1 && os_family='RedHat'
 echo "${OSTYPE}" | grep -q 'darwin' && os_family='MacOSX'
@@ -84,10 +85,13 @@ if [ "${os_family}" == 'RedHat' ]; then
         os_name='redhat';
     fi
 elif [ "${os_family}" == 'Debian' ]; then
+    os_code="$(grep UBUNTU_CODENAME /etc/os-release | awk -F= '{print $2}')"
     if [ -e '/etc/lsb-release' ] ; then
         major_version="$(grep DISTRIB_RELEASE /etc/lsb-release | awk -F= '{print $2}' | awk -F. '{print $1}')"
         minor_version="$(grep DISTRIB_RELEASE /etc/lsb-release | awk -F= '{print $2}' | awk -F. '{print $2}')"
         os_name="$(grep DISTRIB_ID /etc/lsb-release | awk -F= '{print $2}')"
+        # For oses based on Ubuntu we often need the Ubuntu codename (e.g. repository configuration)
+        grep -q UBUNTU_CODENAME /etc/os-release && os_code="$(grep UBUNTU_CODENAME /etc/os-release | awk -F= '{print $2}')"
     else
         major_version="$(awk -F. '{print $1}' /etc/debian_version)"
         minor_version="$(awk -F. '{print $2}' /etc/debian_version)"
@@ -1626,13 +1630,13 @@ function get_env {
     declare -a env_files
     env_files=('/etc/environment' '/etc/profile')
     for env_file in "${env_files[@]}"; do
-	if [ -e "${env_file}" ]; then
-	    debug 10 "Sourcing ${env_file}"
+        if [ -e "${env_file}" ]; then
+            debug 10 "Sourcing ${env_file}"
             #shellcheck source=/dev/null
-	    source "${env_file}"
+            source "${env_file}"
         else
-	    debug 10 "Env file: ${env_file} not present"
-	fi
+            debug 10 "Env file: ${env_file} not present"
+        fi
     done
 }
 
