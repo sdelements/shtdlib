21f32dc90254ad707ea9c458a87bda1fc2919b0f
Antonia Stevens <a@antevens.com>
2019-01-31 17:09:15 -0500

    debug

--- shtdlib.sh
+++ shtdlib.sh
@@ -647,7 +647,7 @@ function priv_esc_with_env {
 function signal_processor {
     local signal="${1}"
     local command="${*:2}"
-    bash -c "trap '${command}' ${signal} && while true; do sleep 1 ; done" &> /dev/stdout &
+    bash -c "trap '${command}' ${signal} && while true; do sleep 1 ; done" &> /dev/null &
     echo "${!}"
 }
 
@@ -2315,7 +2315,8 @@ function test_add_on_mod {
     while pgrep -P ${$} > /dev/null ; do
         debug 10 "Waiting for PID ${signaler_pid} to exit"
         ps -Afl
-        wait ${signaler_pid} &> /dev/stdout
+        set +o errexit
+        wait "${signaler_pid}" &> /dev/null
         return_status="${?}"
         # Make sure the sub process exits with 42
         if [ "${return_status}" != '42' ] ; then
