9425cd36e80dfc52bf3e206ab4b141192040da8c
Antonia Stevens <antonia.s.stevens@gmail.com>
2019-02-01 13:50:02 -0500

    Feature/add import function (#39)
    Add import function.

--- shtdlib.sh
+++ shtdlib.sh
@@ -331,6 +331,49 @@ function test_decorator {
     return 1
 }
 
+# Imports/Sources an external script if it's not already been imported/sourced
+# or is being imported/sourced as determined by BASH_SOURCE
+# Only accepts one argument, the file to source
+declare -a sourced_imported_files
+sourced_imported_files=()
+function import {
+    assert test -n "${0}"
+    assert test -e "${0}"
+    local hasher
+    if whichs shasum; then
+        hasher='shasum'
+    elif whichs md5sum; then
+        hasher='md5sum'
+    elif whichs cksum; then
+        hasher='cksum'
+    else
+        debug 1 "Unable to find a valid hashing command, blindly importing/sourcing!"
+        # shellcheck disable=1090
+        source "${1}" && return 0
+    fi
+    # Create a hash of the target file
+    target_file_hash="$("${hasher}" "${1}")"
+
+    # Add all files in source history to the list of imported files
+    for source_file in "${BASH_SOURCE[@]}"; do
+        source_file_hash="$("${hasher}" "${source_file}" | awk '{print $0}')"
+        if ! in_array "${source_file_hash}" "${sourced_imported_files[@]}" ; then
+            sourced_imported_files+=( "${source_file_hash}" )
+        fi
+    done
+
+    # Check if file has already been sourced/imported
+    if in_array "${target_file_hash}" "${sourced_imported_files[@]}" ; then
+        debug 5 "Source file ${1} has already been imported/sourced, skipping"
+        return 1
+    fi
+
+    # Finally import/source the file if needed
+    debug 7 "Sourcing file ${1}"
+    sourced_imported_files+=( "${target_file_hash}" )
+    # shellcheck disable=1090
+    source "${1}" && return 0
+}
 
 # A platform (readlink implementation) neutral way to follow symlinks
 function readlink_m {
@@ -2166,7 +2209,8 @@ function manage_service {
         debug 10 "Checking command, '${command}', to determine if we can run it on this system"
 
         # Check if the path to the command exists
-        local path=$(echo "${command}" | cut -d' ' -f1)
+        local path
+        path="$(echo "${command}" | cut -d' ' -f1)"
         if [[ -e "${path}" ]]; then
             debug 10 "Path to command found: '${path}'"
 
