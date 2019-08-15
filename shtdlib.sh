2b4597630d598c723afdbd332d72e93c48e655d5
Antonia Stevens <astevens@securitycompass.com>
2019-08-15 14:34:30 -0400

    DEBUG

--- shtdlib.sh
+++ shtdlib.sh
@@ -2590,7 +2590,9 @@ function test_add_on_mod {
     tmp_file_path="$(mktemp)"
     add_on_exit "rm -f ${tmp_file_path}"
     debug 10 "Using temporary file: ${tmp_file_path} to test add_on_mod"
+    echo BEFORE
     max_frequency=5 add_on_mod "signal_process ${signaler_pid} SIGUSR1 &> /dev/null" "${tmp_file_path}" &
+    echo DONE
     mod_watcher_pid="${!}"
     bash -c "sleep 2 && echo 'test message' > '${tmp_file_path}'"
     bash -c "sleep 10 && kill ${signaler_pid} &> /dev/null" &
