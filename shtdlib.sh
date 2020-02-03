8549986929419e36606800af56c487745b674bb4
Matthew Chum <mchum@securitycompass.com>
2020-02-03 16:48:46 -0500

    adding default for test_shtdlib

--- shtdlib.sh
+++ shtdlib.sh
@@ -143,6 +143,7 @@ fi
 
 # Store local IP addresses (not localhost), if it is not virtualized in Docker
 # shellcheck disable=SC2046
+local_ip_addresses=''
 if [ "${virt_platform}" != 'Docker' ]; then
     local_ip_addresses="$( ( (whichs ip && ip -4 addr show) || (whichs ifconfig && ifconfig) || awk '/32 host/ { print "inet " f } {f=$2}' <<< \"$(</proc/net/fib_trie)\") | grep -v 127. | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | sort -u)"
 fi
